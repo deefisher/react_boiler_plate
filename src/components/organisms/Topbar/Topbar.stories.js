77c7797700ea5f07f0fab21afd73408cf1f5c08f
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-10-01 16:24:12 +0100

    implemented Topbar story

---
+++ src/components/organisms/Topbar/Topbar.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Topbar from './Topbar';
+
+export const DefaultTopbar = (props) => <Topbar {...props} />;
+
+const TopbarStory = {
+    title: 'Organisms/Topbar',
+    component: Topbar,
+};
+
+export default TopbarStory;
